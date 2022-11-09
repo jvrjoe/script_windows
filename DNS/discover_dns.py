e20fb347b74aa71205561c98b7a2bc164700c0df
Jonathan Rodrigues <jonathan.rodrigues@protest.com.br>
2022-11-09 16:34:14 -0300

    corrigindo o erro quando o arquivo não é encontrado

--- DNS/discover_dns.py
+++ DNS/discover_dns.py
@@ -1,6 +1,6 @@
 import getpass; import os; from pypsexec.client import Client
 
-print(f'\nCrie um arquivo texto com o nome das máquinas na rede que deseja visualizar.')
+print(f'\nCrie um arquivo texto com o nome das máquinas na rede, que deseja visualizar o servidor DNS utilizado.')
 print(f'Neste arquivo, o nome das máquinas devem estar separados por linha.\n')
 print(f'Exemplo:\n PC1\n PC2\n PC3\n')
 input(f'Com o arquivo criado, pressione a tecla Enter para continuar...\n')
@@ -9,7 +9,9 @@ print(f'É necessário um usuário com privilégios administrativos na rede ou a
 username = input('Nome de usuário: ')
 password = getpass.getpass('Senha: ')
 
-fileHosts = input('Informe o caminho para o arquivo texto criado: ')
+fileHosts = input('\n Agora informe o caminho do arquivo texto criado: ')
+while not os.path.isfile(fileHosts):
+    fileHosts = input("Arquivo não encontrado! Insira corretamente o caminho completo do arquivo texto: ")
 
 with open(fileHosts) as file:
     for host in file:
