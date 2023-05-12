0e1c3264334c862506980ca0ca7b78aca57862b2
Jonathan Rodrigues <jonathan.rodrigues@protest.com.br>
2023-05-12 15:23:53 -0300

    *

--- ocs_inventory/install_ocs.py
+++ ocs_inventory/install_ocs.py
@@ -4,7 +4,7 @@ from pypsexec.client import Client
 
 # Variáveis
 executable= 'cmd'
-argument= r'/c \\pt55\ocs_inventory$\invent.bat'
+argument= r'/c \\90.0.0.9\netlogon\invent.bat'
 
 # print('Instalação remota OCS_Inventory')
 # print('Deve ser executado com login administrativo!')
@@ -19,7 +19,7 @@ print()
 #opt = input('\nEscolha uma opção: \n1 - Instalação individual \n2 - Instalação em lote \n\nSua escolha: ')
 
 #if opt == '1':
-host = 'copa-dp16' #input('Informe o nome da máquina: ')
+host = input('Informe o nome da máquina: ')
 #c = Client(host, username=((domain)+'\\'+(usr)), password=pwd, encrypt=True)
 c = Client(server=host, username=usr, password=pw, encrypt=True)
 
@@ -38,19 +38,3 @@ if rc < 1:
     print(stdout.decode('latin'))
 else:
     print(stderr.decode('latin'))
-    
-
-
-"""host = input('\n Local do arquivo texto com o nome das máquinas: ')
-with open(fileHosts) as file:
-    for host in file:
-       c = Client(host.replace('\n', ''), username=username, password=password, encrypt=True)
-      executable= 'cmd.exe /c \\copinha\\netlogon\invent.bat'
-    argument= ''
-        c.connect()
-        try:
-           c.create_service()
-          stdout, stderr, rc = c.run_executable(executable=executable, arguments=None)
-        finally:
-           c.remove_service()
-            c.disconnect()"""
