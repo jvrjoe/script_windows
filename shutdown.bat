9ceb2c947b38203cf97922b8c857e4f0ee297e96
Jonathan Rodrigues <jonathan.rodrigues@protest.com.br>
2023-05-23 10:07:34 -0300

    update

--- shutdown.bat
+++ shutdown.bat
@@ -1,7 +1,7 @@
-echo off
+@echo off
 chcp 65001
 
-:menu_opcao
+:menu_opt
 cls
 echo:
 echo             MENU DE OPÇÕES             
@@ -12,17 +12,18 @@ echo *   0 - Sair                          *
 echo =======================================
 echo:
 set "opcao="
-set INPUT=false
 set /p opcao="Escolha uma opção: "
-if %opcao% == 1 goto menu_reiniciar
-if %opcao% == 2 goto menu_desligar
-if %opcao% == 0 goto sair
-if %INPUT% == false echo Opção inválida. Tente novamente & timeout 2 > NUL & goto menu_opcao
+if "%opcao%" == "1" goto menu_reboot
+if "%opcao%" == "2" goto menu_shutdown
+if "%opcao%" == "0" ( goto close) else ( echo Opção inválida)
+if "%opcao%" == " " goto menu_opt
+pause
+call :menu_opt
 
-:sair
+:close
 exit
 
-:menu_reiniciar
+:menu_reboot
 cls
 echo:
 echo               REINICIAR             
@@ -34,14 +35,15 @@ echo *   0 - Sair                          *
 echo =======================================
 echo:
 set "opcao="
-set INPUT=false
 set /p opcao="Escolha uma opção: "
-if %opcao% == 1 goto reboot_now
-if %opcao% == 2 goto reboot_after
-if %opcao% == 3 goto menu_opcao
-if %opcao% == 0 goto sair
-if %INPUT% == false echo Opção inválida. Tente novamente & timeout 2 > NUL & goto menu_reiniciar
-    
+
+if "%opcao%" == "1" goto reboot_now
+if "%opcao%" == "2" goto reboot_after
+if "%opcao%" == "3" goto menu_opt
+if "%opcao%" == "0" ( goto close) else ( echo Opção inválida)
+if "%opcao%" == " " goto menu_reboot
+pause
+call :menu_reboot
 
 :reboot_now
 cls
@@ -53,54 +55,144 @@ echo *       2 - Voltar ao menu anterior   *
 echo *       0 - Sair                      *
 echo =======================================
 echo:
-set "host="
 set "opcao="
-set INPUT=false
 set /p opcao="Escolha uma opção: "
 
-if %opcao% == 1 (
-    set /p host="Nome do computador: " &
-    echo Nome host dentro %host%
-    pause )
-    echo Nome hosta fora %host%
-    pause
-    @REM && start /B shutdown.exe /r /t 60 /m \\%host% 
-
-    @REM start /B shutdown.exe /r /m \\%host% /t 60 
-    @REM     if %ERRORLEVEL > 0 echo O computador não existe ou não pode ser reiniciado.
-    @REM ))
-    @REM if %ERRORLEVEL% > 0 echo O computador não existe ou não pode ser reiniciado) 
-    @REM else echo Reiniciando o compuador remoto
-if %opcao% == 2 goto menu_reiniciar
-if %opcao% == 0 goto sair
-if %INPUT% == false echo Opção inválida. Tente novamente & timeout 2 > NUL & goto reboot_now
-
-
+setlocal ENABLEDELAYEDEXPANSION
+if "%opcao%" == "1" (
+    echo:
+    set /p host="Nome do computador: "
+    shutdown.exe /r /m \\!host! /t 00 >NUL 2>&1
+    if !ERRORLEVEL! == 0 (
+        echo:
+        echo O computador !host! está sendo reiniciado.) else (
+            echo:
+            echo O computador !host! não existe ou não pode ser reiniciado.)
+        pause
+)
+endlocal    
+if "%opcao%" == "2" goto menu_reboot
+if "%opcao%" == "0" ( goto close) else ( echo Opção inválida)
+if "%opcao%" == " " goto reboot_now
 pause
+call :reboot_now
 
-goto menu_opcao
+:reboot_after
+cls
+echo:
+echo      DEFINIR TEMPO PARA REINICIAR      
+echo =======================================
+echo *       1 - Informe o computador      *
+echo *       2 - Voltar ao menu anterior   *
+echo *       0 - Sair                      *
+echo =======================================
+echo:
+set "opcao="
+set /p opcao="Escolha uma opção: "
 
-@REM :reboot_after
-@REM cls
-@REM echo:
-@REM echo Definir tempo para reiniciar
-@REM echo
+setlocal ENABLEDELAYEDEXPANSION
+if "%opcao%" == "1" (
+    echo:
+    set /p host="Nome do computador: "
+    set /p time="Reiniciar em quantos segundos? "
+    shutdown.exe /r /m \\!host! /t !time! >NUL 2>&1
+    if !ERRORLEVEL! == 0 (
+        echo:
+        echo O computador !host! será reiniciado em !time! segundos.) else (
+            echo:
+            echo O computador !host! não existe ou não pode ser reiniciado.)
+        pause
+)
+endlocal    
+if "%opcao%" == "2" goto menu_reboot
+if "%opcao%" == "0" ( goto close) else ( echo Opção inválida)
+if "%opcao%" == " " goto reboot_after
+pause
+call :reboot_after
 
-@REM :menu_desligar
-@REM cls
-@REM echo:
-@REM echo                DESLIGAR             
-@REM echo =======================================
-@REM echo *   1 - Desligar imediatamente        *
-@REM echo *   2 - Definir tempo para desligar   *
-@REM echo *   3 - Voltar ao menu principal      *
-@REM echo *   0 - Sair                          *
-@REM echo =======================================
-@REM echo:
-@REM set opcao=""
-@REM set /p opcao="Escolha uma opção: "
+:menu_shutdown
+cls
+echo:
+echo                DESLIGAR             
+echo =======================================
+echo *   1 - Desligar imediatamente        *
+echo *   2 - Definir tempo para desligar   *
+echo *   3 - Voltar ao menu principal      *
+echo *   0 - Sair                          *
+echo =======================================
+echo:
+set "opcao="
+set /p opcao="Escolha uma opção: "
 
-@REM if %opcao% == 1 goto shutdown_now
-@REM if %opcao% == 2 goto shutdown_after
-@REM if %opcao% == 3 goto menu_opcao
-@REM if %opcao% == 0 goto sair
+if "%opcao%" == "1" goto shutdown_now
+if "%opcao%" == "2" goto shutdown_after
+if "%opcao%" == "3" goto menu_opt
+if "%opcao%" == "0" ( goto close) else ( echo Opção inválida)
+if "%opcao%" == " " goto menu_shutdown
+pause
+call :menu_shutdown
+
+:shutdown_now
+cls
+echo:
+echo         DESLIGAR IMEDIATAMENTE
+echo =======================================
+echo *       1 - Informe o computador      *
+echo *       2 - Voltar ao menu anterior   *
+echo *       0 - Sair                      *
+echo =======================================
+echo:
+set "opcao="
+set /p opcao="Escolha uma opção: "
+
+setlocal ENABLEDELAYEDEXPANSION
+if "%opcao%" == "1" (
+    echo:
+    set /p host="Nome do computador: "
+    shutdown.exe /s /m \\!host! /t 00 >NUL 2>&1
+    if !ERRORLEVEL! == 0 (
+        echo:
+        echo O computador !host! está sendo desligado.) else (
+            echo:
+            echo O computador !host! não existe ou não pode ser desligado.)
+        pause
+)
+endlocal    
+if "%opcao%" == "2" goto menu_shutdown
+if "%opcao%" == "0" ( goto close) else ( echo Opção inválida)
+if "%opcao%" == " " goto shutdown_now
+pause
+call :shutdown_now
+
+:shutdown_after
+cls
+echo:
+echo      DEFINIR TEMPO PARA DESLIGAR      
+echo =======================================
+echo *       1 - Informe o computador      *
+echo *       2 - Voltar ao menu anterior   *
+echo *       0 - Sair                      *
+echo =======================================
+echo:
+set "opcao="
+set /p opcao="Escolha uma opção: "
+
+setlocal ENABLEDELAYEDEXPANSION
+if "%opcao%" == "1" (
+    echo:
+    set /p host="Nome do computador: "
+    set /p time="Desligar em quantos segundos? "
+    shutdown.exe /s /m \\!host! /t !time! >NUL 2>&1
+    if !ERRORLEVEL! == 0 (
+        echo:
+        echo O computador !host! será desligado em !time! segundos.) else (
+            echo:
+            echo O computador !host! não existe ou não pode ser desligado.)
+        pause
+)
+endlocal    
+if "%opcao%" == "2" goto menu_shutdown
+if "%opcao%" == "0" ( goto close) else ( echo Opção inválida)
+if "%opcao%" == " " goto shutdown_after
+pause
+call :shutdown_after
