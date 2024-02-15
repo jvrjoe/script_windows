12960d3c0bd2727643567aa5e295fec1cea384d4
Jonathan Rodrigues <jvr_joe@hotmail.com>
2024-02-15 09:35:37 -0300

    .

--- bat.windows/rename_host.bat
+++ bat.windows/rename_host.bat
@@ -17,7 +17,7 @@ setlocal ENABLEDELAYEDEXPANSION
 	set /p host="Nome da máquina: "
 	set /p new_host="Novo nome: "
 	psexec.exe -u %USERDOMAIN%\%USERNAME% -p %pass% \\!host! cmd /c WMIC ComputerSystem where Name="!host!" call Rename Name="!new_host!"
-		if %ERRORLEVEL% == 0 echo Falha ao renomear.
+		if %ERRORLEVEL% == 0 echo Máquina %host% renomeada para %new_host%
         if not %ERRORLEVEL% == 0 echo Falha ao renomear. Erro: %ERRORLEVEL%. & pause & goto rename
 	echo:
 	pause
