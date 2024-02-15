866ea436bd53eb9288ef0f6ed1abcacb38876e21
Jonathan Rodrigues <jvr_joe@hotmail.com>
2024-02-15 09:32:51 -0300

    update rename_host.bat

--- bat.windows/rename_host.bat
+++ bat.windows/rename_host.bat
@@ -2,28 +2,23 @@
 chcp 65001
 title Renomear máquina na rede
 
-rem cls
-rem set /p pass="Sua senha de rede: "
+:acesso
+cls
+echo:
+set "pass="
+set /p pass="Digite sua senha de rede: "
+if not defined pass goto acesso
 
 
 setlocal ENABLEDELAYEDEXPANSION
 	:rename
 	cls
 	echo:
-	rem set "host="
-	rem set "new_host="
 	set /p host="Nome da máquina: "
 	set /p new_host="Novo nome: "
-
-	psexec.exe -u protest\administrator -p 123protestadministrator \\!host! cmd /c WMIC ComputerSystem where Name="!host!" call Rename Name="!new_host!"
-rem	if !ERRORLEVEL! == 0 (
-rem     echo:
-rem     echo !host! renomeada para !new_host!. ) else (
-   rem         echo:
-      rem      echo Erro ao renomear !host! Código: !ERRORLEVEL!.)
-	
-	rem shutdown.exe /r /m \\!host! /t 00 >NUL 2>&1
-
+	psexec.exe -u %USERDOMAIN%\%USERNAME% -p %pass% \\!host! cmd /c WMIC ComputerSystem where Name="!host!" call Rename Name="!new_host!"
+		if %ERRORLEVEL% == 0 echo Falha ao renomear.
+        if not %ERRORLEVEL% == 0 echo Falha ao renomear. Erro: %ERRORLEVEL%. & pause & goto rename
 	echo:
 	pause
 	goto rename
