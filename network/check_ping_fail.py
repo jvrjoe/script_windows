fc1c462ad4fb0aef02bb5f7455a2553fca264f90
Jonathan Rodrigues <jonathan.rodrigues@protest.com.br>
2024-05-15 17:55:44 -0300

    mexidas no ping.py

--- network/check_ping_fail.py
+++ network/check_ping_fail.py
@@ -1,17 +1,18 @@
 import subprocess, datetime, time, os
 
-date_today = datetime.date.today().strftime("%d-%m-%Y")
-time_now = datetime.datetime.now().time().strftime("%H:%M:%S")
+dateNow = datetime.date.today().strftime("%d-%m-%Y")
+timeNow = datetime.datetime.now().time().strftime("%H:%M:%S")
+date_time = datetime.datetime.now().isoformat(timespec='seconds').replace(':','.')
 
-print(datetime.datetime.now().isoformat(timespec='seconds'))
+print(f'{dateNow}\n{timeNow}\n{date_time}')
 
 host = input('Informe o IP do host: ')
-logfile = f"C:\\ping_fail_{host}_{date_today}_{time_now}.txt"
+logfile = f"C:\\ping_fail_{host}_{dateNow}_{timeNow}.txt"
 
 with open(logfile, "a") as file:
     os.system('cls')
-    file.write(f'Monitoramento iniciado para o host: "{host}" -- {date_today} / {time_now}')
-    print(f'Monitoramento iniciado para o host: "{host}" -- {date_today} / {time_now}')
+    file.write(f'Monitoramento iniciado para o host: "{host}" -- {dateNow} / {timeNow}')
+    print(f'Monitoramento iniciado para o host: "{host}" -- {dateNow} / {timeNow}')
     file.close()
  
 while True:
@@ -21,13 +22,13 @@ while True:
         
         if "Esgotado" in output:
             with open(logfile, "a") as file:
-                file.write("Falha às " + str(time_now) + "\n")
+                file.write("Falha às " + str(timeNow) + "\n")
                 file.close()
 
         time.sleep(1)
 
     except KeyboardInterrupt:
         with open(logfile, "a") as file:
-            file.write('Processo finalizado às {}'.format(time_now))
-        print('Processo finalizado às {}'.format(time_now))
+            file.write('Processo finalizado às {}'.format(timeNow))
+        print('Processo finalizado às {}'.format(timeNow))
         exit()
