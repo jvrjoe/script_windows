74100b64fd3f421fbc495991ddac9aaef05255e2
Jonathan Rodrigues <jonathan.rodrigues@protest.com.br>
2025-03-25 09:33:20 -0300

    delet avg copy

--- bat.windows/avg_copy.bat
+++
@@ -1,3 +0,0 @@
-xcopy "H:\Softwares\Antivirus\avg_install.exe" \\barr\e$\Software\antivirus /Y
-xcopy "H:\Softwares\Antivirus\avg_install.exe" \\nitero\e$\Software\antivirus /Y
-xcopy "H:\Softwares\Antivirus\avg_install.exe" \\tijuc\e$\Software\antivirus /Y
